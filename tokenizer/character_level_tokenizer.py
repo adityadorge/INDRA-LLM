8c7bae5e89892e0cb1c1b671108760253ea93a42
Aditya Dorge <adityadorge07@gmail.com>
2026-07-09 21:12:51 +0530

    Initial Commit

---
+++ tokenizer/character_level_tokenizer.py
@@ -0,0 +1,8 @@
+"""
+Data Collection & Preparation
+--------------------------------------
+Download and prepare the TinyShakespeare dataset. This raw text will be used 
+to build our character-level tokenizer and create the training/validation 
+tensors for the decoder-only GPT model.
+"""
+
